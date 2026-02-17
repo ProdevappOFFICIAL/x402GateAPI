c04f62daf81dd5a24f24eb426124d20aa267543f
ProdevappOFFICIAL <122856789+ProdevappOFFICIAL@users.noreply.github.com>
2026-02-17 22:18:18 +0100

    changed cors url

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: ["https://x402-gaurd-ehic.vercel.app/"],
+    origin: ["https://x402-gaurd.vercel.app/"],
     credentials: true,
   })
 );
